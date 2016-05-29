fb30ddc63d2ad6d425f55d9789bac4be695d6101
David English <david.v.english@gmail.com>
2016-05-29 19:30:45 -0400

    Implemented keypad

--- valve/src/key.tsx
+++ valve/src/key.tsx
@@ -10,7 +10,6 @@ export default class Key extends BaseComponent {
     handleOnClick = () => {
         this.props.onClick(this.props.keyText);
     }
-
     render() {
         return <button onClick={this.handleOnClick} className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary">
             {this.props.keyText}
